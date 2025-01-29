ebff2ca34bdacb91c3484c385d1df7c90e3d08a2
Maddlib <jctfarmer@gmail.com>
2025-01-28 17:11:17 -0800

    LeafNode added

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -29,19 +29,33 @@ class TestTextNode(unittest.TestCase):
     def test_props(self):
         node = HTMLNode("<p>", "val", None, {"href":"https://www.google.com"})
         self.assertEqual(
-            "href = \"https://www.google.com\"", node.props_to_html()
+            " href = \"https://www.google.com\"", node.props_to_html()
         )
 
     def test_props_mult(self):
         node = HTMLNode("<p>", "val", None, {"href":"https://www.google.com", "target": "_blank"})
         self.assertEqual(
-            "href = \"https://www.google.com\" target = \"_blank\"", node.props_to_html()
+            " href = \"https://www.google.com\" target = \"_blank\"", node.props_to_html()
         )
 
-    def test_repr(self):
+    def test_htmlnode_repr(self):
         node = HTMLNode("<p>", "val", None, {"href":"https://www.google.com"})
         self.assertEqual(
             "HTMLNode(<p>, val, None, {'href': 'https://www.google.com'})", repr(node)
+        )
+
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_to_html_link(self):
+        node = LeafNode("p", "val", {"href":"https://www.google.com"})
+        self.assertEqual(
+            "<p href = \"https://www.google.com\">val</p>", node.to_html()
         )
 
 if __name__ == "__main__":
